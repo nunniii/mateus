aa1111af3a56b53052f73f9767134f1322a12c41
Mateus Nunes <mateusnss@protonmail.ch>
2023-08-24 10:35:46 -0300

    Corrigido imagem do mapa

--- src/components/blocks/Switch/Location.tsx
+++ src/components/blocks/Switch/Location.tsx
@@ -1,11 +1,11 @@
 
 import { SvgLocation } from "./SvgLocation"
-
+import  map  from '../../../assets/905480454.jpeg'
 
 export function Location(){
     return (
         <div className="card" id="location-card">
-            <img src="./public/assets/905480454.jpeg" alt="Location" />
+            <img src={map} alt="Location" />
             <div id="sp-i-lc">
                 <p style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}><SvgLocation />&nbsp; Divinópolis, BR</p>
             </div>
