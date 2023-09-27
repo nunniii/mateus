eb91aa140040d50355dd34f5bbcdb432b43a74c6
Mateus Nunes <mateusnss@protonmail.ch>
2023-09-27 12:49:38 -0300

    2709

--- src/components/blocks/About.tsx
+++ src/components/blocks/About.tsx
@@ -4,9 +4,9 @@ export function About(){
         <div className="block">
 
               <p>Eu sou um entusiasta de código aberto, com foco em desenvolvimento web.</p>
-              <p>Minhas linguagens de programação favoritas são Rust, TypeScript e atualmente, estou me dedicando ao estudo de WebAssembly, React e React Native. Além disso, tenho interesse em OCaml. Gosto de criar sites rápidos e eficientes.</p>
+              <p>Minhas linguagens de programação de preferência são Rust, C/C++, TypeScript e um pouco de Kotlin e atualmente, estou me dedicando ao estudo de WebAssembly e desenvolvimento mobile.</p>
 
-              <p>Sinta-se à vontade para entrar em contato para assuntos profissionais. Se você encontrar interesses de pesquisa em comum ou oportunidades de trabalho conjunto, não hesite em enviar um <a href="mailto:mateusnss@proton.me">email</a>. Estou sempre aberto a colaborações. uwu</p>
+              <p>Sinta-se à vontade para entrar em contato para assuntos profissionais. Se você encontrar interesses de pesquisa em comum, não hesite em enviar um <a href="mailto:mateusnss@proton.me">email</a>. Estou sempre aberto a colaborações. uwu</p>
               
               <br/><br/>
 
