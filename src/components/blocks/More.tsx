353fd92cced56d9b0d96283bc4b9c302fca9ad68
Mateus Nunes <mateusnss@protonmail.ch>
2024-09-11 23:05:51 -0300

    :p

--- src/components/blocks/More.tsx
+++ src/components/blocks/More.tsx
@@ -2,7 +2,7 @@ export function More(){
     return(
         <div className="block">
               <h2>Mais + contato</h2>
-              <p>Quer conversar sobre algo? Envie um email para <a href="mailto:mateusnss@proton.me">mateusnss@proton.me</a> &#40;<a href="https://gist.githubusercontent.com/nunniii/f9a43361913bad126df4c1f93d8c580c/raw/9da6151b281beaa294500c8efe2813013b489144/gistfile1.txt">Encrypt it</a>&#41; .</p>
+              <p>Quer conversar sobre algo? Envie um email para <a href="mailto:mateusnss@proton.me">mateusnss@proton.me</a> &#40;<a href="https://gist.githubusercontent.com/nunniii/f9a43361913bad126df4c1f93d8c580c/raw/551b29b28f445c4dfc6df0a4cb84495a99480117/gistfile1.txt">Encrypt it</a>&#41; .</p>
               <p>Primary ETH Address: &#40;<span style={{color:'#2563eb'}} className="code">0xa14863710db4ad5Cc54CC4e2e4E4FD26fD75A509</span>&#41;</p>
           </div>
     )
